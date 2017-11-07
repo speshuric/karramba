dc4adcfffa64295f94c0ae16123c7afd33741858
Alexander Speshilov <speshuric@gmail.com>
2017-11-08 02:40:56 +0300

    update init subvolumes

--- scripts/set-subvolumes.sh
+++ scripts/set-subvolumes.sh
@@ -10,43 +10,43 @@ ROOT_PART=sda4
 # Mount subvolid=0 subvolume
 TMP_MNT=/tmp/mnt
 mkdir -p ${TMP_MNT}
-mount /dev/${ROOT_PART} -o subvolid=0 ${TMP_MNT}
+sudo mount /dev/${ROOT_PART} -o subvolid=0 ${TMP_MNT}
 ############################
 
 # Create the main snapshotted subvolume of the root filesystem
-btrfs subvolume create ${TMP_MNT}/@
+sudo btrfs subvolume create ${TMP_MNT}/@
 
 # Create the non-snapshotted subvolumes
 
-btrfs subvolume create ${TMP_MNT}/@/.snapshots
-btrfs subvolume create ${TMP_MNT}/@/opt
-btrfs subvolume create ${TMP_MNT}/@/srv
-btrfs subvolume create ${TMP_MNT}/@/tmp
+sudo btrfs subvolume create ${TMP_MNT}/@/.snapshots
+sudo btrfs subvolume create ${TMP_MNT}/@/opt
+sudo btrfs subvolume create ${TMP_MNT}/@/srv
+sudo btrfs subvolume create ${TMP_MNT}/@/tmp
 
-mkdir -p ${TMP_MNT}/@/boot/grub2 && \
-btrfs subvolume create ${TMP_MNT}/@/boot/grub2/i386-pc && \
-btrfs subvolume create ${TMP_MNT}/@/boot/grub2/x86_64-efi
+sudo mkdir -p ${TMP_MNT}/@/boot/grub2 && \
+sudo btrfs subvolume create ${TMP_MNT}/@/boot/grub2/i386-pc && \
+sudo btrfs subvolume create ${TMP_MNT}/@/boot/grub2/x86_64-efi
 
-mkdir -p ${TMP_MNT}/@/usr && btrfs subvolume create ${TMP_MNT}/@/usr/local
+sudo mkdir -p ${TMP_MNT}/@/usr && sudo btrfs subvolume create ${TMP_MNT}/@/usr/local
 
-mkdir -p ${TMP_MNT}/@/var && \
-btrfs subvolume create ${TMP_MNT}/@/var/cache && \
-btrfs subvolume create ${TMP_MNT}/@/var/crash && \
-btrfs subvolume create ${TMP_MNT}/@/var/log && \
-btrfs subvolume create ${TMP_MNT}/@/var/opt && \
-btrfs subvolume create ${TMP_MNT}/@/var/spool && \
-btrfs subvolume create ${TMP_MNT}/@/var/tmp && \
-btrfs subvolume create ${TMP_MNT}/@/var/abs
+sudo mkdir -p ${TMP_MNT}/@/var && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/cache && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/crash && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/log && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/opt && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/spool && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/tmp && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/abs
 
-mkdir -p ${TMP_MNT}/@/var/lib/libvirt && btrfs subvolume create ${TMP_MNT}/@/var/lib/libvirt/images
+sudo mkdir -p ${TMP_MNT}/@/var/lib/libvirt && sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/libvirt/images
 
-mkdir -p ${TMP_MNT}/@/var/lib && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/machines && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/mailman && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/mariadb && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/mysql && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/named && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
+sudo mkdir -p ${TMP_MNT}/@/var/lib && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/machines && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/mailman && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/mariadb && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/mysql && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/named && \
+sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
 
 # https://wiki.archlinux.org/index.php/Snapper:
 # Keeping many of snapshots for a large timeframe on a busy filesystem like /, where many system updates happen over
@@ -57,13 +57,16 @@ btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
 # WAT???
 # mkdir -p ${TMP_MNT}/@ && btrfs subvolume create ${TMP_MNT}/@/pkg
 
+sudo btrfs subvolume delete /tmp/mnt/var/lib/machines/
+
+
 
 # Create a place to keep snapshots and create an initial snapshot
-mkdir ${TMP_MNT}/@/.snapshots/1
-btrfs subvolume snapshot ${TMP_MNT}/@ ${TMP_MNT}/@/.snapshots/1/snapshot
+sudo mkdir ${TMP_MNT}/@/.snapshots/1
+sudo btrfs subvolume snapshot ${TMP_MNT}/@ ${TMP_MNT}/@/.snapshots/1/snapshot
 
 # Find the ID of the initial snapshot and set it to be the default to be mounted
-defaultsv="$(btrfs subvolume list -o ${TMP_MNT}/@/.snapshots/1 | gawk '$NF ~ "1/snapshot$" {print $2}')"
+defaultsv="$(sudo btrfs subvolume list -o ${TMP_MNT}/@/.snapshots/1 | gawk '$NF ~ "1/snapshot$" {print $2}')"
 btrfs subvolume set-default $defaultsv ${TMP_MNT}/@/.snapshots/1/snapshot
 
 # Finished - unmount complete filesystem
