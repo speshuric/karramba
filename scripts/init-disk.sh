b3548d3c8d1faab5a5277cf35846124ede1b842e
Alexander Speshilov <speshuric@gmail.com>
2017-11-20 03:34:29 +0300

    remove sudo

--- scripts/init-disk.sh
+++ scripts/init-disk.sh
@@ -28,59 +28,56 @@ parted --script /dev/${HDD} -- \
     name 5 home \
     print \
 
-sudo partprobe /dev/${HDD}
+partprobe /dev/${HDD}
 
-sudo mkfs.ext4 /dev/${HDD}2 -L boot
-sudo mkswap /dev/${HDD}3 -L swap
-sudo mkfs.btrfs /dev/${HDD}4 -L root
-sudo mkfs.btrfs /dev/${HDD}5 -L home
+mkfs.ext4 /dev/${HDD}2 -L boot
+mkswap /dev/${HDD}3 -L swap
+mkfs.btrfs /dev/${HDD}4 -L root
+mkfs.btrfs /dev/${HDD}5 -L home
 
 #Idea taken from https://forums.opensuse.org/showthread.php/521277-LEAP-42-2-btrfs-root-filesystem-subvolume-structure
 
 ROOT_PART=${HDD}4
 
-# Create a new btrfs filesystem
-# mkfs.btrfs /dev/${ROOT_PART}
-
 # Mount subvolid=0 subvolume
 TMP_MNT=/tmp/mnt
 mkdir -p ${TMP_MNT}
-sudo mount /dev/${ROOT_PART} -o subvolid=0 ${TMP_MNT}
+mount /dev/${ROOT_PART} -o subvolid=0 ${TMP_MNT}
 ############################
 
 # Create the main snapshotted subvolume of the root filesystem
-sudo btrfs subvolume create ${TMP_MNT}/@
+btrfs subvolume create ${TMP_MNT}/@
 
 # Create the non-snapshotted subvolumes
 
-sudo btrfs subvolume create ${TMP_MNT}/@/opt
-sudo btrfs subvolume create ${TMP_MNT}/@/srv
-sudo btrfs subvolume create ${TMP_MNT}/@/tmp
+btrfs subvolume create ${TMP_MNT}/@/opt
+btrfs subvolume create ${TMP_MNT}/@/srv
+btrfs subvolume create ${TMP_MNT}/@/tmp
 
-sudo mkdir -p ${TMP_MNT}/@/boot/grub2 && \
-sudo btrfs subvolume create ${TMP_MNT}/@/boot/grub2/i386-pc && \
-sudo btrfs subvolume create ${TMP_MNT}/@/boot/grub2/x86_64-efi
+mkdir -p ${TMP_MNT}/@/boot/grub2 && \
+btrfs subvolume create ${TMP_MNT}/@/boot/grub2/i386-pc && \
+btrfs subvolume create ${TMP_MNT}/@/boot/grub2/x86_64-efi
 
-sudo mkdir -p ${TMP_MNT}/@/usr && sudo btrfs subvolume create ${TMP_MNT}/@/usr/local
+mkdir -p ${TMP_MNT}/@/usr && btrfs subvolume create ${TMP_MNT}/@/usr/local
 
-sudo mkdir -p ${TMP_MNT}/@/var && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/cache && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/crash && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/log && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/opt && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/spool && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/tmp && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/abs
+mkdir -p ${TMP_MNT}/@/var && \
+btrfs subvolume create ${TMP_MNT}/@/var/cache && \
+btrfs subvolume create ${TMP_MNT}/@/var/crash && \
+btrfs subvolume create ${TMP_MNT}/@/var/log && \
+btrfs subvolume create ${TMP_MNT}/@/var/opt && \
+btrfs subvolume create ${TMP_MNT}/@/var/spool && \
+btrfs subvolume create ${TMP_MNT}/@/var/tmp && \
+btrfs subvolume create ${TMP_MNT}/@/var/abs
 
-sudo mkdir -p ${TMP_MNT}/@/var/lib/libvirt && sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/libvirt/images
+mkdir -p ${TMP_MNT}/@/var/lib/libvirt && btrfs subvolume create ${TMP_MNT}/@/var/lib/libvirt/images
 
-sudo mkdir -p ${TMP_MNT}/@/var/lib && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/machines && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/mailman && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/mariadb && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/mysql && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/named && \
-sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
+mkdir -p ${TMP_MNT}/@/var/lib && \
+btrfs subvolume create ${TMP_MNT}/@/var/lib/machines && \
+btrfs subvolume create ${TMP_MNT}/@/var/lib/mailman && \
+btrfs subvolume create ${TMP_MNT}/@/var/lib/mariadb && \
+btrfs subvolume create ${TMP_MNT}/@/var/lib/mysql && \
+btrfs subvolume create ${TMP_MNT}/@/var/lib/named && \
+btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
 
 # https://wiki.archlinux.org/index.php/Snapper:
 # Keeping many of snapshots for a large timeframe on a busy filesystem like /, where many system updates happen over
@@ -91,17 +88,17 @@ sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
 # WAT???
 # mkdir -p ${TMP_MNT}/@ && btrfs subvolume create ${TMP_MNT}/@/pkg
 
-#sudo btrfs subvolume delete /tmp/mnt/var/lib/machines/
+#btrfs subvolume delete /tmp/mnt/var/lib/machines/
 
 
-#sudo btrfs subvolume create ${TMP_MNT}/@/.snapshots
+#btrfs subvolume create ${TMP_MNT}/@/.snapshots
 # Create a place to keep snapshots and create an initial snapshot
-#sudo mkdir ${TMP_MNT}/@/.snapshots/1
-#sudo btrfs subvolume snapshot ${TMP_MNT}/@ ${TMP_MNT}/@/.snapshots/1/snapshot
+#mkdir ${TMP_MNT}/@/.snapshots/1
+#btrfs subvolume snapshot ${TMP_MNT}/@ ${TMP_MNT}/@/.snapshots/1/snapshot
 
 # Find the ID of the initial snapshot and set it to be the default to be mounted
-#sudo defaultsv="$(sudo btrfs subvolume list -o ${TMP_MNT}/@/.snapshots/1 | gawk '$NF ~ "1/snapshot$" {print $2}')"
-#sudo btrfs subvolume set-default $defaultsv ${TMP_MNT}/@/.snapshots/1/snapshot
+#defaultsv="$(btrfs subvolume list -o ${TMP_MNT}/@/.snapshots/1 | gawk '$NF ~ "1/snapshot$" {print $2}')"
+#btrfs subvolume set-default $defaultsv ${TMP_MNT}/@/.snapshots/1/snapshot
 
 #mount -o subvol=@/var/lib/libvirt/images /dev/sdb2 /mnt/newfs/var/lib/libvirt/images
 #mount -o subvol=@/var/lib/mariadb /dev/sdb2 /mnt/newfs/var/lib/mariadb
@@ -110,9 +107,9 @@ sudo btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
 #mount -o subvol=@/var/log /dev/sdb2 /mnt/newfs/var/log && mkdir /var/log/journal
 #chattr +C /mnt/newfs/var/lib/libvirt/images  /mnt/newfs/var/lib/mariadb /mnt/newfs/var/lib/pgsql /mnt/newfs/var/lib/mysql /var/log/journal
 
-sudo mkdir ${TMP_MNT}/@/var/log/journal
+mkdir ${TMP_MNT}/@/var/log/journal
 
-sudo chattr +C \
+chattr +C \
     ${TMP_MNT}/@/var/lib/libvirt/images \
     ${TMP_MNT}/@/var/lib/mariadb \
     ${TMP_MNT}/@/var/lib/pgsql \
@@ -121,7 +118,7 @@ sudo chattr +C \
 
 
 # Finished - unmount complete filesystem
-sudo umount ${TMP_MNT}
+umount ${TMP_MNT}
 
 #TODO Исправить calamares
 
