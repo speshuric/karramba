23b139d9ed2da8e743aa6c8b79db6dfd75eba114
Alexander Speshilov <speshuric@gmail.com>
2018-06-04 23:10:20 +0300

    create small script to partitioning

---
+++ scripts/init-disk-small.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# use blkid or lsblck to check what disk you destroy
+
+HDD=sda
+
+partprobe /dev/${HDD}
+parted --script /dev/${HDD} print
+
+parted --script /dev/${HDD} -- \
+    unit MiB \
+    mklabel gpt \
+    mkpart primary 1MiB 4MiB \
+    set 1 bios_grub on \
+    name 1 bios_grub \
+    mkpart primary linux-swap 4MiB 6GiB \
+    name 3 swap \
+    mkpart primary btrfs 6GiB 50GiB \
+    name 4 root \
+    mkpart primary btrfs 50GiB 92% \
+    name 5 home \
+    print \
+
+partprobe /dev/${HDD}
+
+mkswap /dev/${HDD}2 -L swap
+mkfs.btrfs /dev/${HDD}3 -L root
+mkfs.btrfs /dev/${HDD}4 -L home
+
+#Idea taken from https://forums.opensuse.org/showthread.php/521277-LEAP-42-2-btrfs-root-filesystem-subvolume-structure
+
