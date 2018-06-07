ea1349a6edc6d75a2bbfdf460956262a49418bf0
Alexander Speshilov <speshuric@gmail.com>
2018-06-08 02:19:17 +0300

    update init disk

--- scripts/init-disk.sh
+++ scripts/init-disk.sh
@@ -13,18 +13,23 @@ parted --script /dev/${HDD} -- \
     mkpart primary 1MiB 4MiB \
     set 1 bios_grub on \
     name 1 bios_grub \
-    mkpart primary linux-swap 4MiB 6GiB \
-    name 2 swap \
-    mkpart primary btrfs 6GiB 50GiB \
-    name 3 root \
-    mkpart primary btrfs 50GiB 92% \
-    name 4 home \
+    mkpart primary ext4 4MiB 4GiB \
+    set 2 boot on \
+    set 2 esp on \
+    name 2 boot \
+    mkpart primary linux-swap 4GiB 32GiB \
+    name 3 swap \
+    mkpart primary btrfs 32GiB 80GiB \
+    name 4 root \
+    mkpart primary btrfs 80GiB 92% \
+    name 5 home \
     print \
 
 partprobe /dev/${HDD}
 
-mkswap /dev/${HDD}2 -fL swap
-mkfs.btrfs /dev/${HDD}3 -fL root
-mkfs.btrfs /dev/${HDD}4 -fL home
+mkfs.ext4 /dev/${HDD}2 -L boot
+mkswap /dev/${HDD}3 -L swap
+mkfs.btrfs /dev/${HDD}4 -L root
+mkfs.btrfs /dev/${HDD}5 -L home
 
-#Idea taken from https://forums.opensuse.org/showthread.php/521277-LEAP-42-2-btrfs-root-filesystem-subvolume-structure
+
