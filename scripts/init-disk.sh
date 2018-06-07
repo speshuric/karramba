f72393eb53592f7f7e8816cb3277ac62554a5a02
Alexander Speshilov <speshuric@gmail.com>
2018-06-08 02:29:22 +0300

    edit init disk

--- scripts/init-disk.sh
+++ scripts/init-disk.sh
@@ -10,26 +10,31 @@ parted --script /dev/${HDD} print
 parted --script /dev/${HDD} -- \
     unit MiB \
     mklabel gpt \
+ \
     mkpart primary 1MiB 4MiB \
     set 1 bios_grub on \
     name 1 bios_grub \
-    mkpart primary ext4 4MiB 4GiB \
-    set 2 boot on \
+ \
+    mkpart primary fat32 4MiB 512MiB \
     set 2 esp on \
-    name 2 boot \
+    set 2 boot on \
+    name 2 esp\
+\ 
+    mkpart primary ext4 512MiB 5GiB \
+    name 3 boot \
     mkpart primary linux-swap 4GiB 32GiB \
-    name 3 swap \
+    name 4 swap \
     mkpart primary btrfs 32GiB 80GiB \
-    name 4 root \
+    name 5 root \
     mkpart primary btrfs 80GiB 92% \
-    name 5 home \
+    name 6 home \
     print \
 
 partprobe /dev/${HDD}
 
-mkfs.ext4 /dev/${HDD}2 -L boot
-mkswap /dev/${HDD}3 -L swap
-mkfs.btrfs /dev/${HDD}4 -L root
-mkfs.btrfs /dev/${HDD}5 -L home
+# mkfs.ext4 /dev/${HDD}2 -L boot
+# mkswap /dev/${HDD}3 -L swap
+# mkfs.btrfs /dev/${HDD}4 -L root
+# mkfs.btrfs /dev/${HDD}5 -L home
 
 
