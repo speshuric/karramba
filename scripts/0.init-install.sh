0bf8b84b585c210305bbe9ad0f01274033c6df22
Alexander Speshilov <speshuric@gmail.com>
2018-07-08 02:09:29 +0300

    addpacman mirrors

--- scripts/0.init-install.sh
+++ scripts/0.init-install.sh
@@ -7,3 +7,5 @@ fi
 
 # ls -R /usr/share/kbd/keymaps | grep "map.gz" | sed 's/\.map\.gz//g' | sort
 loadkeys us
+sudo pacman-mirrors --fasttrack --api --protocol https --timeout 1
+
--- scripts/set-mirrors.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd $(dirname $0)
-pwd
-sudo cp ../files/etc/pacman.d/mirrorlist.hardcoded /etc/pacman.d/mirrorlist
-sudo cp ../files/etc/pacman.d/antergos-mirrorlist.hardcoded /etc/pacman.d/antergos-mirrorlist
