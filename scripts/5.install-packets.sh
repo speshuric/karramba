13dd56db79ad20cbe1eba94e47164073a6c5eb67
Alexander Speshilov <speshuric@gmail.com>
2018-08-03 03:20:24 +0300

    install grub fixed

--- scripts/5.install-packets.sh
+++ scripts/5.install-packets.sh
@@ -10,9 +10,9 @@ HDD=sda
 #install base
 
 #choose kernels
-CHOSEN_KERNELS=linux414 linux 418 
-CHOSEN_AUR=yaourt base-devel 
-CHOSEN_KERNEL_MODULES=linux414-headers linux414-virtualbox-guest-modules linux418-headers linux418-virtualbox-guest-modules 
+#CHOSEN_KERNELS=linux414 linux418 
+#CHOSEN_AUR=yaourt base-devel 
+#CHOSEN_KERNEL_MODULES=linux414-headers linux414-virtualbox-guest-modules linux418-headers linux418-virtualbox-guest-modules 
 
 
 manjaro-chroot /mnt "grub-install --target=i386-pc --recheck /dev/${HDD}"
