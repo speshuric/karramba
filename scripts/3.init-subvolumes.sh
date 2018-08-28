c2099d2b5de593e3a531dff59fc767996e0e2006
Alexander Speshilov <speshuric@gmail.com>
2018-08-29 01:13:34 +0300

    update subvol scheme

--- scripts/3.init-subvolumes.sh
+++ scripts/3.init-subvolumes.sh
@@ -8,6 +8,7 @@ fi
 #Subvolume list taken from 
 # - https://forums.opensuse.org/showthread.php/521277-LEAP-42-2-btrfs-root-filesystem-subvolume-structure
 # - installed OpenSuse fstab
+# - https://en.opensuse.org/SDB:BTRFS#Default_Subvolumes
 
 ROOT_PART=sda5
 
@@ -28,32 +29,20 @@ btrfs subvolume create ${TMP_MNT}/@
 btrfs subvolume create ${TMP_MNT}/@/opt
 btrfs subvolume create ${TMP_MNT}/@/srv
 btrfs subvolume create ${TMP_MNT}/@/tmp
+btrfs subvolume create ${TMP_MNT}/@/root
+mkdir -p ${TMP_MNT}/@/usr && btrfs subvolume create ${TMP_MNT}/@/usr/local
+btrfs subvolume create ${TMP_MNT}/@/var
+btrfs subvolume create ${TMP_MNT}/@/.snapshots
+# Create a place to keep snapshots and create an initial snapshot
+#mkdir ${TMP_MNT}/@/.snapshots/1
+btrfs subvolume set-default ${TMP_MNT}/@
 
 # boot is another partition - do not create subvolumes
 # mkdir -p ${TMP_MNT}/@/boot/grub2 && \
 # btrfs subvolume create ${TMP_MNT}/@/boot/grub2/i386-pc && \
 # btrfs subvolume create ${TMP_MNT}/@/boot/grub2/x86_64-efi
 
-mkdir -p ${TMP_MNT}/@/usr && btrfs subvolume create ${TMP_MNT}/@/usr/local
 
-mkdir -p ${TMP_MNT}/@/var && \
-btrfs subvolume create ${TMP_MNT}/@/var/cache && \
-btrfs subvolume create ${TMP_MNT}/@/var/crash && \
-btrfs subvolume create ${TMP_MNT}/@/var/log && \
-btrfs subvolume create ${TMP_MNT}/@/var/opt && \
-btrfs subvolume create ${TMP_MNT}/@/var/spool && \
-btrfs subvolume create ${TMP_MNT}/@/var/tmp && \
-btrfs subvolume create ${TMP_MNT}/@/var/abs
-
-mkdir -p ${TMP_MNT}/@/var/lib/libvirt && btrfs subvolume create ${TMP_MNT}/@/var/lib/libvirt/images
-
-mkdir -p ${TMP_MNT}/@/var/lib && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/machines && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/mailman && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/mariadb && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/mysql && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/named && \
-btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
 
 # https://wiki.archlinux.org/index.php/Snapper:
 # Keeping many of snapshots for a large timeframe on a busy filesystem like /, where many system updates happen over
@@ -66,13 +55,12 @@ btrfs subvolume create ${TMP_MNT}/@/var/lib/pgsql
 
 #btrfs subvolume delete /tmp/mnt/var/lib/machines/
 
-btrfs subvolume create ${TMP_MNT}/@/.snapshots
+#btrfs subvolume create ${TMP_MNT}/@/.snapshots
 
-# Create a place to keep snapshots and create an initial snapshot
-mkdir ${TMP_MNT}/@/.snapshots/1
-btrfs subvolume snapshot ${TMP_MNT}/@ ${TMP_MNT}/@/.snapshots/1/snapshot
+
+#btrfs subvolume snapshot ${TMP_MNT}/@ ${TMP_MNT}/@/.snapshots/1/snapshot
 #btrfs subvolume set-default ${TMP_MNT}/@/.snapshots/1/snapshot ${TMP_MNT}/@/.snapshots/1/snapshot
-btrfs subvolume set-default ${TMP_MNT}/@/.snapshots/1/snapshot
+#btrfs subvolume set-default ${TMP_MNT}/@/.snapshots/1/snapshot
 
 # Find the ID of the initial snapshot and set it to be the default to be mounted
 #defaultsv="$(btrfs subvolume list -o ${TMP_MNT}/@/.snapshots/1 | gawk '$NF ~ "1/snapshot$" {print $2}')"
@@ -85,14 +73,14 @@ btrfs subvolume set-default ${TMP_MNT}/@/.snapshots/1/snapshot
 #mount -o subvol=@/var/log /dev/sdb2 /mnt/newfs/var/log && mkdir /var/log/journal
 #chattr +C /mnt/newfs/var/lib/libvirt/images  /mnt/newfs/var/lib/mariadb /mnt/newfs/var/lib/pgsql /mnt/newfs/var/lib/mysql /var/log/journal
 
-mkdir ${TMP_MNT}/@/var/log/journal
+#mkdir ${TMP_MNT}/@/var/log/journal
 
-chattr +C \
-    ${TMP_MNT}/@/var/lib/libvirt/images \
-    ${TMP_MNT}/@/var/lib/mariadb \
-    ${TMP_MNT}/@/var/lib/pgsql \
-    ${TMP_MNT}/@/var/lib/mysql \
-    ${TMP_MNT}/@/var/log/journal
+#chattr +C \
+#    ${TMP_MNT}/@/var/lib/libvirt/images \
+#    ${TMP_MNT}/@/var/lib/mariadb \
+#    ${TMP_MNT}/@/var/lib/pgsql \
+#    ${TMP_MNT}/@/var/lib/mysql \
+#    ${TMP_MNT}/@/var/log/journal
 
 #btrfs subvolume set-default ${TMP_MNT}/@
 
