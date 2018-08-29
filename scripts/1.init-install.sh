1d1bd50e021091554ec6eb7e16fbca3ea3691dc1
Alexander Speshilov <speshuric@gmail.com>
2018-08-29 22:30:02 +0300

    update pacman-mirrors selector

--- scripts/1.init-install.sh
+++ scripts/1.init-install.sh
@@ -7,7 +7,7 @@ fi
 
 # ls -R /usr/share/kbd/keymaps | grep "map.gz" | sed 's/\.map\.gz//g' | sort
 loadkeys us
-sudo pacman-mirrors --fasttrack --api --protocol https --timeout 1
+sudo pacman-mirrors --api --protocol https --timeout 1
 
 # для установщика manjaro
 touch /tmp/.btrfsroot
