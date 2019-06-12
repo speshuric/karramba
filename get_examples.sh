5e86aca66e4b6f817f96780a96e6695ccd07576d
Alexander Speshilov <speshuric@gmail.com>
2019-06-12 17:38:56 +0300

    fix examples folder

--- get_examples.sh
+++ get_examples.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
+
+function git_update {
+    if [ -d "$1" ]; then
+        pushd $1 > /dev/null
+        echo "$1:"
+        git pull --recurse-submodules
+        popd > /dev/null
+    else 
+        echo "https://github.com/$1.git : "
+        git clone --recurse-submodules "https://github.com/$1.git"  "$1"
+    fi
+}
+
 mkdir -p ./examples
-cd ./examples
-git clone https://github.com/pigmonkey/spark.git
-git clone https://github.com/jahrik/ansible-arch-workstation.git
-git clone https://github.com/v0rn/ansible-arch.git
+pushd ./examples > /dev/null
+git_update id101010/ansible-archlinux
+git_update pigmonkey/spark
+git_update jahrik/ansible-arch-workstation
+git_update v0rn/ansible-arch
+popd > /dev/null
