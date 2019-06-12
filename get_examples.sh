40930e577be5381eca9d7fd56727649adaf7e7ef
Alexander Speshilov <speshuric@gmail.com>
2019-06-12 14:57:55 +0300

    add examples folder

---
+++ get_examples.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mkdir -p ./examples
+cd ./examples
+git clone https://github.com/pigmonkey/spark.git
+git clone https://github.com/jahrik/ansible-arch-workstation.git
+git clone https://github.com/v0rn/ansible-arch.git
